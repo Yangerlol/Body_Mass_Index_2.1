86a65221c0d746ed90041edf81051ab163b663d5
Yangerlol <yanis4rtuna@gmail.com>
2025-12-12 15:37:41 +0300

    g

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class BmiService {
+
+    // Метод принимает вес (в кг) и рост (в метрах)
+    // и возвращает BMI (дробное число - double)
+    public double calculate(double weightKg, double heightM) {
+
+        // Рост в квадрате (heightM * heightM)
+        double heightSq = heightM * heightM;
+
+        // Формула: Вес / Рост^2
+        double bmi = weightKg / heightSq;
+
+        return bmi;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        // 1. Создание объекта сервиса
+        BmiService service = new BmiService();
+
+        // Входные данные:
+        double weight = 98; // Вес в килограммах (например, 98 кг)
+        double height = 1.87; // Рост в метрах (например, 1 метр 87 см)
+
+        // 2. Вызов метода calculate и получение результата
+        double bmi = service.calculate(weight, height);
+
+        // 3. Печать результата в консоль
+        System.out.println("Ваш Индекс Массы Тела (BMI): " + bmi);
+    }
 }
